41287b67979d27954ff621b7333da765cc395e38
Sepehr Saemi <sepehrsaemi@hotmail.com>
2025-09-06 11:45:31 +0300

    feat: save and load the setting toggle status from browser storage.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,8 @@
+// HTML Elements
+const settingToggleEl = document.querySelector("#setting-toggle");
+const headerIconEl = document.querySelector("#header-icon");
+const unhookContentEl = document.querySelector("#unhook-content");
+
 function getBrowserInfo() {
   // Detect the user's browser type
   if (typeof browser !== "undefined") {
@@ -24,7 +29,42 @@ const platforms = {
   youtube: loadYoutubeUI,
 };
 
-function loadYoutubeUI() {
-  // Load YouTube's specific popup UI
+function loadPopupUI() {
+  settingToggleEl.children[1].removeAttribute("disabled");
+
+  // Load setting toggle status
+  api.storage.sync.get(["settingToggle"] || [], (data) => {
+    if (data.settingToggle) {
+      settingToggleEl.children[0].textContent = data.settingToggle;
+    } else {
+      settingToggleEl.children[0].textContent = "Off";
+    }
+
+    if (data.settingToggle === "On") {
+      settingToggleEl.children[1].setAttribute("checked", "checked");
+    }
+  });
 }
 
+function loadYoutubeUI() {
+  // Load YouTube's specific popup UI
+  loadPopupUI();
+
+  headerIconEl.innerHTML = ``;
+  unhookContentEl.innerHTML = ``;
+}
+
+settingToggleEl.addEventListener("change", () => {
+  // Save the setting toggle status to storage
+  let status = settingToggleEl.children[0].textContent;
+
+  if (status === "Off") {
+    settingToggleEl.children[0].textContent = "On";
+    status = "On";
+  } else {
+    settingToggleEl.children[0].textContent = "Off";
+    status = "Off";
+  }
+
+  api.storage.sync.set({ settingToggle: status });
+});
