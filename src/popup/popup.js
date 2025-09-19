da88388c516243edac9fa91e8430854423a3fb06
sepehrsaemi <232699108+sepehrsaemi@users.noreply.github.com>
2025-09-19 16:21:53 +0300

    Detect second-level domain and load platform-specific popup UI
    Detect the active tab's second-level domain and load a platform-specific
    popup UI, if the platform supports.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,9 @@
+// HTML Elements
+const headerIconEl = document.querySelector("#header-icon");
+const unhookContentEl = document.querySelector("#unhook-content");
+
+let sld;
+
 function getBrowserInfo() {
   // Detect user browser type
   if (typeof browser !== "undefined") {
@@ -8,3 +14,25 @@ function getBrowserInfo() {
 }
 
 const api = getBrowserInfo();
+
+// Get second-level domain of active tab
+api.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  const url = tabs[0].url;
+  sld = url.replace(/(https:\/\/)|www\.|\.[^.]*$/g, "");
+
+  // Load platform-specific UI based on SLD
+  if (platforms[sld]) {
+    platforms[sld]();
+  }
+});
+
+const platforms = {
+  youtube: loadYoutubeUI,
+};
+
+function loadYoutubeUI() {
+  // Load YouTube's specific popup UI
+  headerIconEl.src = "";
+
+  unhookContentEl.innerHTML = ``;
+}
