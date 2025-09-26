c87e0249db8f7cbb9150eb0f18a8845e64917fcb
sepehrsaemi <232699108+sepehrsaemi@users.noreply.github.com>
2025-09-27 01:38:11 +0300

    Add the 'Available Soon' message for X, Instagram, and Twitch

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -35,6 +35,9 @@ api.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
 
 const platforms = {
   youtube: loadYoutubeUI,
+  x: loadTwitterUI,
+  instagram: loadInstagramUI,
+  twitch: loadTwitchUI,
 };
 
 async function saveOptions(platform, id) {
@@ -211,6 +214,45 @@ function loadYoutubeUI() {
       saveOptions("youtube", el.id);
     });
   });
+}
+
+function loadTwitterUI() {
+  loadPopup();
+
+  unhookContentEl.innerHTML = `
+    <div role="alert" class="alert alert-info">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span class="font-bold">Available Soon</span>
+    </div>
+  `;
+}
+
+function loadInstagramUI() {
+  loadPopup();
+
+  unhookContentEl.innerHTML = `
+    <div role="alert" class="alert alert-info">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span class="font-bold">Available Soon</span>
+    </div>
+  `;
+}
+
+function loadTwitchUI() {
+  loadPopup();
+
+  unhookContentEl.innerHTML = `
+    <div role="alert" class="alert alert-info">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span class="font-bold">Available Soon</span>
+    </div>
+  `;
 }
 
 optCtrlEl.addEventListener("change", optionController);
